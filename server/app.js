e3a291e4de35d42f24cc0419323b7271fc91e072
JesusGuerrero <rocha.jesus@hotmail.com>
2018-11-15 16:19:09 -0800

    Timer Physical System

--- server/app.js
+++ server/app.js
@@ -1,56 +1,101 @@
 // Use Cases:
-//   1. User sets up the tine
-//   2. User reads time left
-//   3. When times up, Alarm song rings
-//   4.Press button to turn off the alarm
-
+//   1. User adds time to timer
+//   2. User reads timer preset
+//   3. User start timer
+//   4. User stops/pauses timer
+//   5. When times up alert is display, sound emitted & text message sent
+//   6. User turns off system or resets the timer
 
 var five = require("johnny-five");
-var board = new five.Board(), timer = 5;
 var songs = require('j5-songs');
-var alert = false, start = false;
+
+var board = new five.Board();
+var alert = false,
+  startTimer = false,
+  piezo, lcd, stopStartButton, addTimeButton, addTimeButtonClicks = 0, inputTimer, inputTimeout = 1000,
+  timer = 0, currentTime, setTime;
+
+var processCommand = function(){
+  switch( addTimeButtonClicks ) {
+    case 2: // add 30 seconds
+      timer = timer + 30;
+      break;
+    case 3: // add one minute
+      timer = timer + 60;
+      break;
+    case 4: // add 5 minutes
+      timer = timer + 60 * 5;
+      break;
+    default:
+      timer = timer + 1;
+      break;
+  }
+  displayTime('Add Time');
+};
+var resetTimer = function() {
+  inputTimer = null;
+  addTimeButtonClicks = 0;
+};
+var displayTime = function( msg ) {
+  currentTime = new Date();
+  setTime = new Date();
+  setTime.setSeconds( currentTime.getSeconds() + timer );
+
+  var lengthTime = setTime - currentTime;
+  var hours = Math.floor((lengthTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((lengthTime % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((lengthTime % (1000 * 60)) / 1000);
+
+  lcd.clear();
+  lcd.cursor(0,0).print( msg );
+  if( lengthTime > 0) {
+    lcd.cursor(1,0).print(hours + "h "
+      + minutes + "m " + seconds + "s ");
+  } else {
+    lcd.cursor(1,0).print('Time Expired');
+    if( !alert ) {
+      piezo.play( songs.load('mario-intro') );
+      alert = true;
+    }
+  }
+};
 
 board.on("ready", function() {
-  var piezo = new five.Piezo(10);
-  var stopStartAlert = new five.Button({
+  piezo = new five.Piezo(10);
+  lcd = new five.LCD({
+    controller: "PCF8574T"
+  });
+  stopStartButton = new five.Button({
     pin: 2,
     isPullup: true
   });
-  var setTime = new five.Button({
+  stopStartButton.on('press', function(){
+    startTimer = !startTimer;
+  });
+  stopStartButton.on('hold', function() {
+    resetTimer();
+    startTimer = false;
+    timer = 0;
+    alert = false;
+  });
+  addTimeButton = new five.Button({
     pin: 3,
     isPullup: true
   });
-
-  var l = new five.LCD({
-    controller: "PCF8574T"
-  });
-
-  stopStartAlert.on('press', function(){
-    if( alert ) {
-      alert = false;
-    } else {
-      start = true;
+  addTimeButton.on('press', function() {
+    addTimeButtonClicks += 1;
+    if( !inputTimer ) {
+      inputTimer = setTimeout(function(){
+        processCommand(1);
+        resetTimer();
+      }, inputTimeout);
     }
   });
-  setTime.on('press', function() {
-    timer = timer + 10;
-  });
-
-  setInterval( function(){
-    if( start ) {
-      if( timer >= 0 ) {
-        var n = ('0' + timer--).slice(-2);
-        l.cursor(0, 0).print("Time Left 00:" + n + '           ');
-        if( timer === 0 ) { alert = true; }
-      } else {
-        if( alert ) {
-          l.cursor(0, 0).print('TIMER ENDS');
-          piezo.play( songs.load('mario-intro') );
-        }
-      }
-    } else {
-      var n = ('0' + timer).slice(-2);
-      l.cursor(0, 0).print("Set Time 00:" + n + '           ');
-    }
-  }, 1000);
 });
+setInterval( function() {
+  currentTime = new Date();
+  if( startTimer ) {
+    timer = timer - 1;
+    displayTime('Time Remaining');
+  }
+}, 1000);
